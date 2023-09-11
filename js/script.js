978c480616cad0b015838c74f746448782761173
Jmukumzoda <jmukumzoda63@gmail.com>
2023-09-11 13:11:49 +0500

    update_button_color

--- js/script.js
+++ js/script.js
@@ -14,14 +14,15 @@
 // });
 
 let inps = document.querySelectorAll("input")
-let p = document.querySelector('.p1')
+let p = document.querySelector('.p3')
+let p1 = document.querySelector('.p2')
 let form = document.querySelector('form')
 let patterns = {
   name: /^[a-z ,.'-]+$/i,
   surename: /^[a-z ,.'-]+$/i,
   phone: /^\+998([- ])?(90|91|93|94|95|98|99|33|97|71)([- ])?(\d{3})([- ])?(\d{2})([- ])?(\d{2})$/i,
   email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i,
-  age:/^\S[0-9]{0,3}$/i
+  age: /^\S[0-9]{0,3}$/i
 }
 inps.forEach(ipn => {
   ipn.onkeyup = () => {
@@ -29,14 +30,15 @@ inps.forEach(ipn => {
       ipn.style.borderColor = 'blue'
       ipn.style.overflow = 'blue'
       ipn.classList.remove('error')
-      p.style.color = 'blue'
+      document.querySelector("button").style.background = '#4200FF'
     } else {
       ipn.style.borderColor = 'red'
       ipn.classList.add('error')
       ipn.style.overflow = 'red'
-      p.style.color = 'blue'
+      document.querySelector("button").style.background = '#EE0004'
     }
-   
+    
+
   }
 
 })
